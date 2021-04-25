a0e546576f2cf2c95dc1d9a0131538fd12e36e12
Ivo Kolev <arkevo@Ivos-MacBook-Pro.local>
2021-04-25 00:59:58 -0400

    more server.js debug

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var port = process.env.PORT || 8077;
+
 app.use(express.static(path.join(__dirname, '/')));
 
 // viewed at http://localhost:8078
@@ -17,8 +19,9 @@ app.get('/index2.html', (req, res) => {
     res.sendFile('/index2.html', { root: __dirname });
 });
 
-const port = process.env.PORT || 8077;
-
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 
 
   
